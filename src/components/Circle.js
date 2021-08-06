3583c86010a036f8c49d081ca823bb9392d22d6f
Steffan van de Poll <steffanvandepoll@gmail.com>
2021-08-06 16:13:40 +0200

    Better form of factory

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,18 +1,15 @@
 import Phaser from 'phaser';
-import coinImg from '../assets/img/coin.png';
 
-export default class Circle extends Phaser.Physics.Arcade.Image {
-  constructor(scene, x, y) {
-    super(scene, x, y, 'coin');
-    scene.add.existing(this);
-    scene.physics.world.enable(this);
-    this.body.velocity.setTo(Math.random() * 50 + 100, Math.random() * 50 + 100);
-    this.body.bounce.setTo(1, 1);
-    this.body.collideWorldBounds = true;
-    this.body.interactive = true;
-  }
-  
-  preload() {
-    this.load.spritesheet('coin', coinImg, { frameWidth: 22, frameHeight: 22 });
+export default class Circle {
+  constructor(scene, x, y, callback) {
+
+    this.scene = scene;
+    this.body = this.scene.physics.add.image(Math.random() * 800, Math.random() * 600, 'coin');
+
+    this.body.setVelocity(Math.random() * 50 + 100, Math.random() * 150 + 50);
+    this.body.setBounce(1, 1);
+    this.body.setCollideWorldBounds(true);
+    this.body.setInteractive();
+    this.body.on('pointerdown', callback);
   }
 }
--- src/scenes/playTheGameScene.js
+++ src/scenes/playTheGameScene.js
@@ -19,38 +19,23 @@ export default class playTheGame extends Phaser.Scene {
     this.load.image('bg', bg);
     this.load.spritesheet('coin', coinImg, { frameWidth: 22, frameHeight: 22 });
     this.load.spritesheet('big_coin', coinImg, { frameWidth: 150, frameHeight: 150 });
-
   }
 
   create() {
     this.add.image(0, 0, 'bg').setOrigin(0, 0).setScale(0.78);
 
-    var customBounds = new Phaser.Geom.Rectangle(0, 0, 800, 600);
-
     this.coins = [];
 
     for(var i = 0; i < 50; i++){
-      // let coin = this.physics.add.image(Math.random() * 800, Math.random() * 600, 'coin');
-
-      let coin = new Circle(this, Math.random() * 800, Math.random() * 600, 'coin');
-
-      
-      this.coins.push(coin);
-
-      this.physics.add.existing(coin);
-  
+      let coin = new Circle(this, Math.random() * 800, Math.random() * 600, this.addExplosion);
     }
-
-
-
-    //this.physics.add.collider(this.player, platforms);
-    //this.physics.add.collider(this.coin, platforms);
-    //this.physics.add.overlap(this.player, this.coin, this.collectCoin, null, this);
-
-    //this.keys = this.input.keyboard.addKeys(getKeys());
   }
 
   update() {
    
+  }
+
+  addExplosion() {
+    console.log('boem');
   }
 }
